a52a70d8841d93616257f820784d5594c0ab5412
Tim Landwerth <tim.landwerth@posteo.de>
2020-10-29 17:06:35 +0100

    Display grid

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <Board :height="8" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Board from './components/Board.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Board
   }
 }
 </script>
---
+++ src/components/Board.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="grid-container">
+    <template class="grid-item" v-for="row in boardArray">
+      <div v-for="column in row">
+        {{ column }}
+      </div>
+    </template>
+  </div>
+  <button @click="onClick">Klick mich!</button>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue';
+
+export default {
+  name: 'Board',
+  props: {
+    width: {
+      type: Number,
+      default: 10,
+    },
+    height: {
+      type: Number,
+      default: 10,
+    },
+  },
+  setup(props) {
+    const boardArray = ref(new Array(props.width).fill(
+      new Array(props.height).fill(false)
+    ));
+
+    const onClick = () => { boardArray.value = [true, ...boardArray.value] };
+
+    return {
+      boardArray,
+      onClick,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(10, 50px);
+}
+
+.grid-item {
+
+}
+</style>
